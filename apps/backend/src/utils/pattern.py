76ebf8577f603fb59d54a310b064c077604c8bf5
Mr. Soros Photchara <photcharasoros1@gmail.com>
2025-08-20 19:36:55 +0700

    Update pattern.py

--- apps/backend/src/utils/pattern.py
+++ apps/backend/src/utils/pattern.py
@@ -1,3 +1,4 @@
 class Pattern:
     FINDING_ALL_BRANDS = r'<li>\s*<a href="([^"]*phones[^"]*)">(.*?)</a>\s*</li>'
     FINDING_ALL_PHONE_BRAND = r'<li>\s*<a href="([^"]*\.php)">\s*<img src="([^"]*\.(?:jpg|png|webp))"[^>]*>\s*(.*?)</a>\s*</li>'
+    FINDING_TABLE_HEAD = r'<th[^>]*rowspan="[^"]*"[^>]*scope="row"[^>]*>(.*?)</th>'
