708dfffe7617c974364da941795434d23328a935
Mr. Soros Photchara <photcharasoros1@gmail.com>
2025-08-20 20:20:33 +0700

    add pattern

--- apps/backend/src/utils/pattern.py
+++ apps/backend/src/utils/pattern.py
@@ -2,3 +2,5 @@ class Pattern:
     FINDING_ALL_BRANDS = r'<li>\s*<a href="([^"]*phones[^"]*)">(.*?)</a>\s*</li>'
     FINDING_ALL_PHONE_BRAND = r'<li>\s*<a href="([^"]*\.php)">\s*<img src="([^"]*\.(?:jpg|png|webp))"[^>]*>\s*(.*?)</a>\s*</li>'
     FINDING_TABLE_HEAD = r'<th[^>]*rowspan="[^"]*"[^>]*scope="row"[^>]*>(.*?)</th>'
+    FINDING_TABLE_TTL = r'<td[^>]*class="ttl"[^>]*>(.*?)</td>'
+    FINDING_TABLE_NFO = r'<td[^>]*class=["\'][^"\']*\bnfo\b[^"\']*["\'][^>]*>([\s\S]*?)</td>'
