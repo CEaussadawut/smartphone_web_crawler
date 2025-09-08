c93b1792a6a58e4e82ea602b27cd7893064f9081
Mr. Soros Photchara <photcharasoros1@gmail.com>
2025-09-08 19:45:04 +0700

    clean a branch

--- apps/backend/src/model/__init__.py
+++ apps/backend/src/model/__init__.py
@@ -1,8 +1,8 @@
 from src.model.brands import Brands
 from src.model.phone import PhonePreview
-
+from src.model.spec import SpecPhone
 __all__ = [
     "Brands",
     "PhonePreview",
-    "SpecPhone"
+    "SpecPhone",
 ]
